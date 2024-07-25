bcf638f05d6f992f3513d7a016d98f6aee7bf4e4
Allenreinoso28 <Allen.j.reinoso@gmail.com>
2024-07-25 19:16:22 -0400

    created base pieces of HTML for user interaction

--- yt-to-mp3/src/App.js
+++ yt-to-mp3/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1> hello</h1> 
+  
+      <div class="top-container">
+        <form action="convert-mp3" method="post" id="form">
+
+          <h1><i class="fa-brands fa-square-youtube"></i>
+            Youtube to MP3 Converter</h1>
+          <input id="videoID" type="text" placeholder="Youtube Link"></input><button id="covert-btn">Convert</button>
+        </form>
+      </div>
     </div>
   );
 }
