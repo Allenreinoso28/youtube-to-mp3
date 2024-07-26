ba0e5f4f55d30d8cbdc5348a43d7bf1fd3fb921f
Allenreinoso28 <Allen.j.reinoso@gmail.com>
2024-07-26 12:54:28 -0400

    Moving data over to a jsx file type
    making the move over to the jsx file type to follow the format that was used in the to_do_app workshop i took at school

---
+++ yt-to-mp3/src/components/Form.jsx
@@ -0,0 +1,7 @@
+{/* <form method='post' onSubmit={handleSubmit}id="videoIDForm">
+
+          <h1><i class="fa-brands fa-square-youtube"></i>
+            Youtube to MP3 Converter</h1>
+          <input id="videoID" type="text" placeholder="Youtube Link"></input><button type='submit' id="covert-btn">Convert</button>
+          <div>{FormData}</div>
+        </form> */}
