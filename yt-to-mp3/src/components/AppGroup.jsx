68e65560ce42315d33a2c9ccdfb470e81774b269
Allenreinoso28 <Allen.j.reinoso@gmail.com>
2024-07-26 16:40:52 -0400

    implemented JSX and Yt2Mp3 API

--- yt-to-mp3/src/components/AppGroup.jsx
+++ yt-to-mp3/src/components/AppGroup.jsx
@@ -1,1 +1,62 @@
-import React, {useState} from "react";
+// Importing the 'React' module and 'useState' hook from the 'react' library.
+//import React, {useState} from "react";
+//Import other functions
+import Form from "./Form"
+function AppGroup() {
+    
+    const getLink = async (link) => {
+        console.log(link);
+  
+        var urlLink = link;
+        var videoid = urlLink.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+        if(videoid != null) {
+        
+        console.log("video id = ",videoid[1]);
+        console.log(videoid[1]);
+        
+        const url = `https://youtube-mp36.p.rapidapi.com/dl?id=${videoid[1]}`;
+        console.log(url);
+        const options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': 'd93378e5abmshcfcb677c9c44a1cp1f8d79jsn95ace4640607',
+                'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com'
+            }
+        };
+        
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+            console.log(result.title);
+            console.log(result.author);
+            console.log(result.thumb);
+            console.log(result.link);
+        } catch (error) {
+            console.error(error);
+        }
+
+
+        } else { 
+            console.log("The youtube url is not valid.");
+        }
+        
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return (
+      <div className="App">
+        <Form getLink={getLink}/>
+        
+      </div>
+    );
+  }
+  
+  export default AppGroup;
--- yt-to-mp3/src/components/Form.jsx
+++ yt-to-mp3/src/components/Form.jsx
@@ -1,7 +1,35 @@
-{/* <form method='post' onSubmit={handleSubmit}id="videoIDForm">
+import React, {useState} from "react";
 
-          <h1><i class="fa-brands fa-square-youtube"></i>
-            Youtube to MP3 Converter</h1>
-          <input id="videoID" type="text" placeholder="Youtube Link"></input><button type='submit' id="covert-btn">Convert</button>
-          <div>{FormData}</div>
-        </form> */}
+const Form = ({getLink}) => {
+  const [link, setLink] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (link !== "") {
+      getLink(link);
+      setLink("");
+    } else {
+      alert("Invalid Link!");
+    }
+  };
+
+  return (
+    <form className="AppForm" onSubmit={handleSubmit}>
+      <h1><i class="fa-brands fa-square-youtube"></i>
+      Youtube to MP3 Converter</h1>
+      <input
+      type="text"
+      className="link-input"
+      placeholder="Youtube Link"
+      value={link}
+      onChange={(e) => setLink(e.target.value)}>
+      </input>
+      <button type="submit" className="submit-btn">Convert</button>
+      <div>{link}</div>
+    </form>
+    
+  );
+};
+
+export default Form;
